0a65f16bd5cbf42dba99ddb5d1876957e08151cc
surabhivrn6@gmail.com <surabhivrn6@gmail.com>
2026-08-25 13:38:13 +0530

    minor changes

--- src/components/AboutSection/AboutSection.jsx
+++ src/components/AboutSection/AboutSection.jsx
@@ -4,9 +4,7 @@ function AboutSection() {
   return (
     <section id='about' className='about-section'>
       <div className='about-wrapper container'>
-        <div className='about-image'>
-          <img src='/Photo.jpeg' alt='Rakesh' />
-        </div>
+        
         <div className='about-copy'>
           <h1>About Me</h1>
           <p>Hi! I'm Surabhi V , a developer based in India building fast, accessible web experiences.</p>
