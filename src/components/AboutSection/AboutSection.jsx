444e00cfd15d3a00a4c3354115f9d293749812cc
surabhivrn6@gmail.com <surabhivrn6@gmail.com>
2026-08-25 16:31:40 +0530

    changes

--- src/components/AboutSection/AboutSection.jsx
+++ src/components/AboutSection/AboutSection.jsx
@@ -7,7 +7,7 @@ function AboutSection() {
         
         <div className='about-copy'>
           <h1>About Me</h1>
-          <p>Hi! I'm Surabhi V , I am a Computer Science Student passionate about technology and coding , I enjoy learning new skills and creating creative projects .</p>
+          <p>Hi! I'm Surabhi V , I am a Computer Science Student passionate about technology and coding , I enjoy learning new skills and creating creative projects I am always excited to explore new opportunities and grow as a developer.</p>
         </div>
       </div>
     </section>
