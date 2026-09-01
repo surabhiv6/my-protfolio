d8cc57bf8f2aa892feb3d01fc97d1c4c0faa5283
surabhivrn6@gmail.com <surabhivrn6@gmail.com>
2026-09-01 14:36:35 +0530

    changes

--- src/components/AboutSection/AboutSection.jsx
+++ src/components/AboutSection/AboutSection.jsx
@@ -1,15 +1,17 @@
 import './AboutSection.css';
+import profileImg from './profile.jpeg'; // Add this import statement
 
 function AboutSection() {
   return (
     <section id='about' className='about-section'>
       <div className='about-wrapper'>
         <div className='about-image'>
-          <img src='/profile.jpeg' alt='Surabhi V' />
+          {/* Change src to use the imported image variable */}
+          <img src={profileImg} alt='Surabhi V' />
         </div>
         <div className='about-copy'>
           <h1>About Me</h1>
-          <p>Hi! I'm Surabhi V, a Computer Science Engineering student constantly learning and building fast, accessible web experiences.</p>
+          <p>Hi! I'm Surabhi V, a Computer Science Engineering student constantly learning and building fast...</p>
         </div>
       </div>
     </section>
